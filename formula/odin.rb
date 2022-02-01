a3d9f30e6e9af593830f0eb05b49fb73080cdf1a
Harshit Sharma <brown.hash@outlook.com>
2022-02-01 14:56:18 +0530

    fix: remove sha

--- formula/odin.rb
+++ formula/odin.rb
@@ -9,13 +9,13 @@ class Odin < Formula
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "0d7e61b9d3e1b2601236efa4d9d9a50fab60574a76fe0cd7a2d5711020d98951"
+    # sha256 "0d7e61b9d3e1b2601236efa4d9d9a50fab60574a76fe0cd7a2d5711020d98951"
   end
 
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "a9a582a321e51becac8df21cfcf8ef30085184ec78698a3a99b5761e539accbb"
+    # sha256 "a9a582a321e51becac8df21cfcf8ef30085184ec78698a3a99b5761e539accbb"
   end
 
   conflicts_with "odin"
