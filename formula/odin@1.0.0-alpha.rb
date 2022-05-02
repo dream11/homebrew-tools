f9d98f642a9bbc6a5a7168c4194c10a5364c479c
Harshit Sharma <brown.hash@outlook.com>
2022-05-02 15:36:52 +0530

    feat: odin 1.0.0-alpha public formula

--- formula/odin@1.0.0-alpha.rb
+++ formula/odin@1.0.0-alpha.rb
@@ -1,5 +1,4 @@
 # typed: false
-require_relative "lib/github"
 
 class OdinAT100Alpha < Formula
   desc "Internal framework CLI for CRUD operations with environments"
@@ -8,22 +7,22 @@ class OdinAT100Alpha < Formula
 
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_darwin_amd64.tar.gz"
     sha256 "ba833d556af4a47923fcb8780ca31492369808eea2165c733f5b5fd25c3885a7"
   end
 
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_darwin_arm64.tar.gz"
     sha256 "4c5d45c7ad41cd217aa9f1bc906b921161a2554cb3e5084f3188ffd347ccc7b9"
   end
 
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/dream11/odin/releases/download/1.0.0-alpha/odin_linux_amd64.tar.gz"
     sha256 "801e95a44248765a1afdab0d650321f286cd6bf506c88ae571db3d0624dac37e"
   end
-  
+
   def install
     bin.install "odin"
   end
