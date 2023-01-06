bd4963872f93c8ac7f55d7212f0d05b0babf3979
Yogesh Badke <yogesh.badke@dream11.com>
2023-01-06 12:14:10 +0530

    release 1.2.3 odin

--- formula/odin.rb
+++ formula/odin.rb
@@ -3,24 +3,24 @@
 class Odin < Formula
   desc "Interface for service definitions & deployments into self-managed environments"
   homepage "https://github.com/dream11/odin"
-  version "1.2.2"
+  version "1.2.3"
 
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/dream11/odin/releases/download/1.2.2/odin_darwin_amd64.tar.gz"
-    sha256 "bf72d9864f42ff2b526dbda1cb623d1ad2552aa6e79a9f797ae72ad98d6e26e3"
+    sha256 "f3c83f1e89456829f8cfb81dc8e4dfa36e8995fd391fbb461e14915e4d3d4440"
   end
 
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/dream11/odin/releases/download/1.2.2/odin_darwin_arm64.tar.gz"
-    sha256 "90879d6ea4a00b6adf54895fb512798a539f7e6048ccf466c960a441987f4fbc"
+    sha256 "2f521eca959c8e2dd27a212baae354e3913808fcb6d3fcf5219c6505b42fc20a"
   end
 
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/dream11/odin/releases/download/1.2.2/odin_linux_amd64.tar.gz"
-    sha256 "cb95b52146238bf429cb1aba49b9c0f3acdd25e83f0113b84c2f42f8d3d3853f"
+    sha256 "3d24dcc6216dc07c6e57c2d5c4230e2d41f0fc7c5a2895857f1fc305e9a0888b"
   end
 
   def install
