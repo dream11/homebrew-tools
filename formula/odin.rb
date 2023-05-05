6d7dfd41244305cbbd8a6c11fa2494b176ee0127
Yogesh Badke <35845927+yogesh-badke@users.noreply.github.com>
2023-05-05 16:51:15 +0530

    Revert "Release odin cli 1.3.2-beta on homebrew tools (#19)"
    This reverts commit b3f5656ab813cb7b363b1e9fd021354a872c3206.

--- formula/odin.rb
+++ formula/odin.rb
@@ -3,24 +3,24 @@
 class Odin < Formula
   desc "Interface for service definitions & deployments into self-managed environments"
   homepage "https://github.com/dream11/odin"
-  version "1.3.2-beta"
+  version "1.3.1-beta"
 
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dream11/odin/releases/download/1.3.2-beta/odin_darwin_amd64.tar.gz"
-    sha256 "4b6bf398e9a16f2e33afada60bb427cc3835086b8d062825a169c4d88ba89674"
+    url "https://github.com/dream11/odin/releases/download/1.3.1-beta/odin_darwin_amd64.tar.gz"
+    sha256 "45ce5dc0f26085d91dcddfe6dd021d9fa28465b043e1bb8b769169f2d0991807"
   end
 
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dream11/odin/releases/download/1.3.2-beta/odin_darwin_arm64.tar.gz"
-    sha256 "9acb9a52f65dea646d105775526713f59c7914d80a8fb0e6c09fdef2d76fb64d"
+    url "https://github.com/dream11/odin/releases/download/1.3.1-beta/odin_darwin_arm64.tar.gz"
+    sha256 "48e39a52194db160d68b93f2ee185ff83bf7af2b2f59cb19df6f7e6ca4a96918"
   end
 
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dream11/odin/releases/download/1.3.2-beta/odin_linux_amd64.tar.gz"
-    sha256 "194154325cc17ba3e54b21accafa5ee59e1e70520125b9d0fc813df91f143f2d"
+    url "https://github.com/dream11/odin/releases/download/1.3.1-beta/odin_linux_amd64.tar.gz"
+    sha256 "9ec270d135f15aace3b4bc49c2f2217fc8bef17ee8912a88fdeeeecca9676b13"
   end
 
   def install
