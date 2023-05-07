5680151beff99a7fe722ec3a84a409af0dcc3465
Umesh Gadage <umesh.gadage@ip-192-168-0-101.ec2.internal>
2023-05-07 19:43:18 +0530

    Updated new sha256 for livelogs version 0.0.1

--- formula/livelogs@0.0.1.rb
+++ formula/livelogs@0.0.1.rb
@@ -8,19 +8,19 @@ class LivelogsAT001 < Formula
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/dream11/livelogs/releases/download/0.0.1/livelogs_darwin_amd64.tar.gz"
-    sha256 "154b3a64c2afdec98578e204c6565862e2879ba5192f8849abc83c69b62d7af1"
+    sha256 "530491f4582d4e13bf574fe6a9cfae2ea4e1bd1a68682c4a6618404dbd37e729"
   end
 
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/dream11/livelogs/releases/download/0.0.1/livelogs_darwin_arm64.tar.gz"
-    sha256 "a1eb796269f3b90b79ec07ec6ed573ceadd04684d81b5ccc6d51d4be5ed23341"
+    sha256 "9e838d22654da1be6f2a2bbc85dfd12c4720911e30659aab947f087ba2599994"
   end
 
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/dream11/livelogs/releases/download/0.0.1/livelogs_linux_amd64.tar.gz"
-    sha256 "41d3837c9f576f547dea2c9f61fa689877d4a1ce5173452e143176a9bb18ef59"
+    sha256 "7110e6f400adcf2fa8cf35819473a139b47626042f060aee9288c81bf14c9f08"
   end
 
   def install
