51d7e0f76711cf5ce549d4f749b50954109c35f8
ruturaj04 <45591579+ruturaj04@users.noreply.github.com>
2024-01-11 12:35:46 +0530

    Update streamverse.rb

--- formula/streamverse.rb
+++ formula/streamverse.rb
@@ -8,19 +8,19 @@ class Streamverse < Formula
 
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dream11/streamverse/releases/download/1.0.0/streamverse_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/dream11/streamverse/releases/download/1.0.0-uat/streamverse_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "764cb63d31d384582f53581f0608341aebe6ce858d24a6d63d4e67a37ed7edfa"
   end
 
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dream11/streamverse/releases/download/1.0.0/streamverse_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/dream11/streamverse/releases/download/1.0.0-uat/streamverse_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "5ad48e7163d1e0710881eb27358914d83c7ff5c314fa06d754d55eb4081b0ec6"
   end
 
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
-    url " https://github.com/dream11/streamverse/releases/download/1.0.0/streamverse_linux_amd64.tar.gz"
+    url " https://github.com/dream11/streamverse/releases/download/1.0.0-uat/streamverse_linux_amd64.tar.gz"
     sha256 "2313d3190587f643a90136dad85a117c290ffb012cff572dc3102c26ae2fa778"
   end
 
