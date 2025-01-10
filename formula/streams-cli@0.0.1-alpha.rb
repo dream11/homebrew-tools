153c22b05e412f2de09dbc7d6a716f216e021c2d
Sohan Honavar <sohan.honavar@dream11.com>
2025-01-10 15:35:38 +0530

    Streams cli 0.0.1-aplha release

---
+++ formula/streams-cli@0.0.1-alpha.rb
@@ -0,0 +1,25 @@
+# typed: false
+require_relative "lib/github"
+
+class Streamverse < Formula
+  desc "Streams CLI"
+  homepage "ttps://github.com/dream11/streams-cli"
+  version "0.0.1-alpha"
+
+
+  # For MacOs M1 based systems
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.1-alpha/streamverse_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "35729391bf3bcaba3b962f79a81c0105eda922f0ffbc7e921f1144b790a61e4b"
+  end
+
+
+  def install
+    bin.install "streams-cli"
+  end
+
+  test do
+    system "#{bin}/streams-cli version"
+  end
+
+end
