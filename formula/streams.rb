b2bcca6d15c82868c6e88f6a8602d4a811c4e147
Soumya Ghosh <83294836+soumya-d11@users.noreply.github.com>
2025-01-10 19:30:17 +0530

    Allow download from private repo (#42)
    * Update streams.rb to downloads from private repo
    
    * Update streams@0.0.5-alpha.rb to allow download from private repo

--- formula/streams.rb
+++ formula/streams.rb
@@ -6,17 +6,17 @@ class Streams < Formula
   version "0.0.5-alpha"
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dream11/streams-cli/releases/download/0.0.5-alpha/streamverse_darwin_amd64.tar.gz"
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.5-alpha/streamverse_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "ce22b8b2a425bb7ce8ea1ce14cd99f6190bd50215799cdfbeb37741cbba9d4ce"
   end
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dream11/streams-cli/releases/download/0.0.5-alpha/streamverse_darwin_arm64.tar.gz"
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.5-alpha/streamverse_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "e450486ca88152bedabb9a3f34557012ba4707496ff9d21be694c29c5a03a0a4"
   end
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dream11/streams-cli/releases/download/0.0.5-alpha/streamverse_linux_amd64.tar.gz"
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.5-alpha/streamverse_linux_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "ea13ec76594da9537027bd13fb0e1b3572d4e5a987473a4ea136eb8eca7b8b7c"
   end
 
