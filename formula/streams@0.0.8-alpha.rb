4d3b2d4824785ef2313141475198f23c2e949d13
cigitbotbackend <cigitbotbackend@dream11.com>
2025-01-14 12:11:17 +0530

    Release streams@0.0.8-alpha (#46)
    * chore: release streams@0.0.8-alpha
    
    * release v0.0.8-alpha
    
    ---------
    
    Co-authored-by: dream11bot <actions@dream11.com>
    Co-authored-by: Sohan Honavar <sohan.honavar@dream11.com>

--- formula/streams@0.0.8-alpha.rb
+++ formula/streams@0.0.8-alpha.rb
@@ -1,7 +1,7 @@
 # typed: false
 require_relative "lib/github"
 
-class Streams < Formula
+class StreamsAT008Alpha < Formula
   desc "Streams cli"
   homepage "https://github.com/dream11/streams-cli"
   version "0.0.8-alpha"
