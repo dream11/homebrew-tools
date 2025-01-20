28fe4caf0befd2f960148d398db322d975c7337f
Sohan Honavar <sohan.honavar@dream11.com>
2025-01-20 15:56:13 +0530

    v 0.0.11-aplha deployment

--- formula/streams.rb
+++ formula/streams.rb
@@ -1,4 +1,5 @@
 # typed: false
+require_relative "lib/github"
 
 class Streams < Formula
   desc "Streams cli"
@@ -6,17 +7,17 @@ class Streams < Formula
   version "0.0.11-alpha"
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dream11/streams-cli/releases/download/0.0.11-alpha/streams_darwin_amd64.tar.gz"
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.11-alpha/streams_darwin_amd64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "8c98b10ccd0f389b4a61ba576d6b64ef552335c865592248b7bc6fadf86c4346"
   end
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dream11/streams-cli/releases/download/0.0.11-alpha/streams_darwin_arm64.tar.gz"
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.11-alpha/streams_darwin_arm64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "a8a46fa1f3b6e47106c7758e243a259fc7fbea297234db34b6c7e0c131d58cea"
   end
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dream11/streams-cli/releases/download/0.0.11-alpha/streams_linux_amd64.tar.gz"
+    url "https://github.com/dream11/streams-cli/releases/download/0.0.11-alpha/streams_linux_amd64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "f3de0dac3688f1ae64c1193af5d65647b82f87d337c44b0b1e426665e815c47a"
   end
 
