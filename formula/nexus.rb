fe6b0e5a84d3260af5d19ae03ec3fd38e974c078
cigitbotbackend <cigitbotbackend@dream11.com>
2025-02-25 16:17:21 +0530

    Release nexus@0.0.13 (#50)
    * chore: release nexus@0.0.13
    
    * First Nexus release
    
    ---------
    
    Co-authored-by: dream11bot <actions@dream11.com>
    Co-authored-by: Sohan Honavar <sohan.honavar@dream11.com>

---
+++ formula/nexus.rb
@@ -0,0 +1,32 @@
+# typed: false
+require_relative "lib/github"
+
+class Nexus < Formula
+  desc "nexus cli"
+  homepage "https://github.com/dream11/nexus-cli"
+  version "0.0.13"
+  # For MacOs Intel based systems
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/dream11/nexus-cli/releases/download/0.0.13/nexus_darwin_amd64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "cbacd6af6339ca7b781b2966344f0a18a48e4081aa861077ba293c95672d5d85"
+  end
+  # For MacOs M1 based systems
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/dream11/nexus-cli/releases/download/0.0.13/nexus_darwin_arm64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "3a10bdc3a5716538c7d7c5b383e065aef1cb14926bfa428b077d578ce9f7cc93"
+  end
+  # For Linux X64 based systems
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/dream11/nexus-cli/releases/download/0.0.13/nexus_linux_amd64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "9c243a76248d9f134ee790906e9f638f14915ec6b092ac2ccd88c3e73c08c530"
+  end
+
+  def install
+    bin.install "nexus"
+  end
+
+  test do
+    system "#{bin}/nexus --version && #{bin}/nexus --help"
+  end
+
+end
