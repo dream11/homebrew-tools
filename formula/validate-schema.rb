7c8a6fe58b92bc6f7f1a8fa3b1e54fc112b97daf
kajald11 <kajal.jain@dream11.com>
2025-04-25 17:09:08 +0530

    Validate schema CLI (#56)
    * Validate schema CLI
    
    * Versioned file

---
+++ formula/validate-schema.rb
@@ -0,0 +1,14 @@
+class ValidateSchema < Formula
+  desc "Internal CLI tool to validate GraphQL schemas"
+  homepage "https://github.com/dream11/validate-schema-cli"
+  url "git@github.com:dream11/validate-schema-cli.git", using: :git, tag: "v1.0.2"
+  version "1.0.2"
+
+  def install
+    bin.install "validateSchema.sh" => "validate-schema"
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/validate-schema --help", 0)
+  end
+end
