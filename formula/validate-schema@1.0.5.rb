160c921f38885a60df6f5c59eecf3bc6b66be609
Kajal Jain <kajal.jain@dream11.com>
2025-04-30 13:34:50 +0530

    Moving download via tar file

--- formula/validate-schema@1.0.5.rb
+++ formula/validate-schema@1.0.5.rb
@@ -1,7 +1,8 @@
 class ValidateSchema < Formula
   desc "Internal CLI tool to validate GraphQL schemas"
   homepage "https://github.com/dream11/validate-schema-cli"
-  url "git@github.com:dream11/validate-schema-cli.git", using: :git, tag: "v1.0.5"
+  url "https://github.com/dream11/validate-schema-cli/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "1.0.5"
 
   def install
