8dc3a5fbadf39150abceb28766bc580b9575aaba
kajald11 <kajal.jain@dream11.com>
2025-04-30 14:21:26 +0530

    Version bump (#60)

--- formula/validate-schema@1.0.5.rb
+++ formula/validate-schema@1.0.5.rb
@@ -1,9 +1,8 @@
 class ValidateSchema < Formula
   desc "Internal CLI tool to validate GraphQL schemas"
   homepage "https://github.com/dream11/validate-schema-cli"
-  url "https://github.com/dream11/validate-schema-cli/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  version "1.0.5"
+  url "https://github.com/dream11/validate-schema-cli.git", :tag => "v1.0.7"
+  version "1.0.7"
 
   def install
     bin.install "validateSchema.sh" => "validate-schema"
