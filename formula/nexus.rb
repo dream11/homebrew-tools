0ab62cc20b9ea912d3e31eb745d63733a497b428
shivamtomar <shivam.tomar@dream11.com>
2025-08-12 21:21:42 +0530

    chore: private repository code added

--- formula/nexus.rb
+++ formula/nexus.rb
@@ -1,4 +1,5 @@
 # typed: false
+require_relative "lib/github"
 
 class Nexus < Formula
   desc "nexus cli"
@@ -6,17 +7,17 @@ class Nexus < Formula
   version "0.0.17"
   # For MacOs Intel based systems
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dream11/nexus-cli/releases/download/0.0.17/nexus_darwin_amd64.tar.gz"
+    url "https://github.com/dream11/nexus-cli/releases/download/0.0.17/nexus_darwin_amd64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "30623c1ac670d0c1e8ac50fad8f63fe0b03fbace7b8237cc080afab7b786e868"
   end
   # For MacOs M1 based systems
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dream11/nexus-cli/releases/download/0.0.17/nexus_darwin_arm64.tar.gz"
+    url "https://github.com/dream11/nexus-cli/releases/download/0.0.17/nexus_darwin_arm64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "c67308afeb20f4a91432a11f9d09e5d1d359df90e58cfdf1980217fa8b56becf"
   end
   # For Linux X64 based systems
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dream11/nexus-cli/releases/download/0.0.17/nexus_linux_amd64.tar.gz"
+    url "https://github.com/dream11/nexus-cli/releases/download/0.0.17/nexus_linux_amd64.tar.gz" , :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "f1294f279143b556db68dd1d76102f7127303af6f1f4c0cef45a38f24ae3d896"
   end
 
